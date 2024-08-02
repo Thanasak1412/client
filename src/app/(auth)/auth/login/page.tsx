7013f255a559e10d67fa680b40e82f37daccf77a
Thanasak1412 <Thanasak.sr60@snru.ac.th>
2024-08-02 09:06:13 +0700

    Update responsive login page

--- src/app/(auth)/auth/login/page.tsx
+++ src/app/(auth)/auth/login/page.tsx
@@ -3,13 +3,11 @@ import { cn } from '@/app/utils/client';
 import LoginForm from '@/app/ui/login-form';
 
 export default function Page() {
-  console.log('render login')
-  
   return (
     <main className="container h-screen py-16">
       <div className="flex flex-row items-center h-full">
         {/* PARAGRAPH SECTION */}
-        <div className="flex flex-col h-full px-6">
+        <div className="flex-col hidden h-full px-6 md:flex">
           <div className="flex flex-col items-center justify-center flex-1 space-y-4">
             <h3 className="text-3xl font-bold leading-6">Hi Welcome back</h3>
 
@@ -21,7 +19,7 @@ export default function Page() {
           <Image
             src="/illustration_login.png"
             alt="Login account"
-            className="flex-[0.3] hidden md:block"
+            className="flex-[0.3]"
             fill
             style={{ objectPosition: 'center' }}
             sizes="(min-width: 1024px) 432px, 100%"
@@ -30,7 +28,7 @@ export default function Page() {
 
         <div
           className={cn(
-            'flex flex-1 items-center justify-center',
+            'flex flex-auto items-center justify-center',
             'md:px-0 md:py-[calc(72px+24px)]'
           )}
         >
